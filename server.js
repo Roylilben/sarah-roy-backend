e4448aff7798b14d28691094359749bb9ee40b29
Roylilben <bensonmasawel33@gmail.com>
2026-03-22 08:52:44 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
 
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// Use environment variable for MongoDB
+mongoose.connect(process.env.MONGO_URL)
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Sarah & Roy Backend Running 🚀");
+});
+
+app.use("/api/auth", require("./routes/auth"));
+
+app.listen(5000, () => console.log("Server running on port 5000"));
